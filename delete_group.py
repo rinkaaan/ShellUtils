6e0cd47835e7c9828a8a27f528f1934a7b0a50aa
Lincoln Nguyen <137611486+rinkaaan@users.noreply.github.com>
2023-12-02 04:02:27 -0800

    Add create_group, delete_group, update delete_group

--- delete_group.py
+++ delete_group.py
@@ -5,23 +5,19 @@ import subprocess
 import click
 from nguylinc_python_utils.misc import run_command
 
-if not os.path.exists(".git"):
-    print("Not a git repository")
-    exit()
 
-result = subprocess.run("git config --file .gitmodules --get-regexp path | awk '{ print $2 }'", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+def get_repos():
+    if not os.path.exists(".git"):
+        print("Not a git repository")
+        exit()
 
-if result.returncode != 0:
-    print("No submodules found")
-    exit()
-
-
-@click.command()
-def main():
-    repos = []
+    result = subprocess.run("git config --file .gitmodules --get-regexp path | awk '{ print $2 }'", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if result.returncode != 0:
+        print("No submodules found")
+        exit()
 
     print("Repositories found: ")
-
+    repos = []
     group_name = os.path.basename(os.getcwd())
     repos.append(group_name)
     print('   ' + group_name)
@@ -32,14 +28,24 @@ def main():
         repos.append(submodule)
         print('   ' + submodule)
 
+    return repos
+
+
+def delete_repos(repos):
     confirmation = click.confirm("Are you sure you want to proceed?")
 
     if confirmation:
-        click.echo("You confirmed! Deleting repos...")
+        click.echo("Deleting repos...")
         for repo in repos:
             run_command("gh repo delete " + repo + " --confirm")
     else:
-        click.echo("You did not confirm. Exiting...")
+        click.echo("Exiting...")
+
+
+@click.command()
+def main():
+    repos = get_repos()
+    delete_repos(repos)
 
 
 if __name__ == "__main__":
---
+++ rename.py
@@ -0,0 +1,44 @@
+#!/opt/homebrew/bin/python3.12
+import os
+import click
+from nguylinc_python_utils.misc import rename_at_root
+
+
+def rename_substring_in_files(root_dir, old_substring, new_substring):
+    for root, dirs, files in os.walk(root_dir):
+        if ".git" in root:
+            continue
+
+        for file in files:
+            # Rename files
+            new_filename = file.replace(old_substring, new_substring)
+            rename_at_root(root, file, new_filename)
+
+            with open(os.path.join(root, new_filename), "r") as f:
+                contents = f.read()
+                new_contents = contents.replace(old_substring, new_substring)
+                with open(os.path.join(root, new_filename), "w") as f2:
+                    f2.write(new_contents)
+
+    for root, dirs, files in os.walk(root_dir):
+        if ".git" in root:
+            continue
+
+        for directory in dirs:
+            # Rename directories
+            new_directory = directory.replace(old_substring, new_substring)
+            rename_at_root(root, directory, new_directory)
+
+
+@click.command()
+@click.argument('root_dir', type=click.Path(exists=True))
+@click.argument('old_substring')
+@click.argument('new_substring')
+def main(root_dir, old_substring, new_substring):
+    rename_substring_in_files(root_dir, old_substring, new_substring)
+    click.echo("Substring renamed successfully!")
+
+
+# python rename.py /Volumes/workplace/WebAppTemplate "{{ project_name }}" "{{ group_name }}"
+if __name__ == "__main__":
+    main()
