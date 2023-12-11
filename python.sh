dfe208cf41dd7474855b74f8ab9f55c0c12b2ce4
Lincoln Nguyen <137611486+rinkaaan@users.noreply.github.com>
2023-12-11 09:04:56 -0800

    Update new-pyenv

--- python.sh
+++ python.sh
@@ -1,11 +1,10 @@
 #pyenv virtualenv 3.12 WebAppTemplate
 #pyenv local WebAppTemplate
 
-# create function that creates pyenv virtualenv using name of current directory
 new-pyenv() {
   current_dir=$(basename "$PWD")
   echo "Creating pyenv virtualenv $current_dir"
-  pyenv virtualenv 3.12 "$current_dir"
+  pyenv virtualenv "${1:-3.12}" "$current_dir"
   pyenv local "$current_dir"
 }
 
