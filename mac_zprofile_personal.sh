024e4258723286f8462f9cf4315ce5f08f6276ce
Lincoln Nguyen <137611486+rinkaaan@users.noreply.github.com>
2024-02-04 00:43:40 -0800

    Add mac_zprofile_personal.sh

---
+++ mac_zprofile_personal.sh
@@ -0,0 +1,16 @@
+# ln -sf $HOME/workplace/Utils/ShellUtils/mac_zprofile_personal.sh $HOME/.zprofile
+
+export PATH="/opt/homebrew/bin:$PATH"
+export PATH="/Users/lincolnnguyen/Library/Application Support/JetBrains/Toolbox/scripts:$PATH"
+
+# Other
+setopt +o nomatch
+alias terminal='open -a Terminal'
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# Python
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
+echo 'eval "$(pyenv init -)"' >> ~/.profile
+
+source ~/startup.sh
